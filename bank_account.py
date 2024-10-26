4baa4562139c94abaf025bc1b1a8dabd676eeafd
David Doherty <froztydavie@Davids-MacBook-Air-3.local>
2024-10-26 15:56:49 -0700

    deposit commit

--- bank_account.py
+++ bank_account.py
@@ -10,8 +10,13 @@ class BankAccount:
         #generate a random 8-digit num
         return random.randint(10000000, 99999999)
     
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Amount deposited: ${amount:.2f} new balance: ${self.balance:.2f}")
+    
 
 account = BankAccount(full_name="David Doherty")
+account.deposit(500.32)
 print(f"Account Owner: {account.full_name}")
 print(f"Account Number: {account.account_number}")
 print(f"Accoount Balance: ${account.balance}")
